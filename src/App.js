f9b36f6dec062c387fe38947a801c8a781b2f5d3
KatarzynaZoladek <katarzynawojcik47@gmail.com>
2024-03-06 13:06:43 +0100

    moving the application to react.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form className="form">
+        <h1 className="form__header">Kalkulator walut</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>
+            <span className="form__labelText"> Kwota *pole obowiązkowe </span>
+            <input
+              className="form__field"
+              required
+              type="number"
+              min="1"
+              step="any"
+              value="100"
+            />
+          </label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label>
+          <span className="form__labelText"> Przelicz z: </span>
+          <select className="form__field" name="currency">
+            <option value="USD">USD</option>
+            <option value="GBP">GBP</option>
+            <option value="EUR" selected>EUR</option>
+          </select>
+        </label>
+        <p>
+          <button className="form__button">Przelicz!</button>
+        </p>
+        <p className="form__result">
+          Kwota w PLN:
+        </p>
+        <p className="form___info">
+          * Przelicznik kursu walut na dzień 18.01.2024.
+        </p>
+      </form>
     </div>
   );
 }
---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.0561,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.1516,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.4166,
+    },
+];
